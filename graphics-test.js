76a2e933d09a1d330381b4d2da8921b2de18a4fb
bbalage <bbalage0@gmail.com>
2021-09-02 12:14:54 +0200

    Incomplete graphics test code.

---
+++ graphics-test.js
@@ -0,0 +1,83 @@
+const canvas = document.querySelector("#c");
+
+const gl =canvas.getContext("webgl2");
+if (!gl) {
+    alert("No WebGL2 context. You cannot play on this browser (or machine).");
+}
+
+const vertexShaderSource = `#version 300 es
+
+in vec4 a_position;
+
+void main() {
+    gl_Position = a_position;
+}`;
+
+const fragmentShaderSource = `#version 300 es
+
+precision highp float;
+
+out vec4 outColor;
+
+void main() {
+    outColor = vec4(1, 0, 0.5, 1);
+}`
+
+function createShader(gl, type, source) {
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (success) {
+        return shader;
+    }
+
+    console.log(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+}
+
+function createProgram(gl, vertexShader, fragmentShader) {
+    const program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    const success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+        return program;
+    }
+
+    console.log(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+}
+
+const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+const program = createProgram(gl, vertexShader, fragmentShader);
+
+const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+const positionBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+const positions = [
+    0, 0,
+    0, 0.5,
+    0.7, 0
+];
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+const vao = gl.createVertexArray();
+gl.bindVertexArray(vao);
+gl.enableVertexAttribArray(positionAttributeLocation);
+
+const size = 2;          // 2 components per iteration
+const type = gl.FLOAT;   // the data is 32bit floats
+const normalize = false; // don't normalize the data
+const stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+const offset = 0;        // start at the beginning of the buffer
+gl.vertexAttribPointer(
+    positionAttributeLocation, size, type, normalize, stride, offset);
+
+gl.viewport(0,0, gl.canvas.width, gl.canvas.height);
+gl.clearColor(0, 0, 0, 0);
+gl.clear(gl.COLOR_BUFFER_BIT);
